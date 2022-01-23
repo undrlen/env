995dc3a4906b3a4cf2c468627d49d1918f1f406c
Nikolay Abubakirov <undrle@yandex.ru>
2022-01-23 14:53:56 +0500

    install babel && polyfill

---
+++ build/main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+var App = function App() {
+  return /*#__PURE__*/React.createElement("h2", null, "Own React");
+};
+
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById("root"));
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => <h2>Own React</h2>;
+
+ReactDOM.render(<App />, document.getElementById("root"));
