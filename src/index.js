b45c26e3f56b795423cc8ce855b08eb3b82b9ea8
Nikolay Abubakirov <undrle@yandex.ru>
2022-01-24 16:45:42 +0500

    add plugins

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "./main.scss";
+import "../public/css/main.css"
+import "./agency.jpg";
+
 const App = () => <h2>Hello, World</h2>;
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,73 @@
 
-module.exports = {
-  mode: "development",
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const devMode = process.env.NODE_ENV !== "production";
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "babel-loader"
-          }
-        ]
-      },
-      {
-        test: /\.(jpg|jpeg|png|ico|gif)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              outputPath: "img",
-              name: "[name]-[sha1:hash:7].[ext]"
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(ttf|woff|woff2|eot|otf)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              outputPath: "fonts",
-              name: "[name].[ext]"
-            }
-          }
-        ]
-      }
-    ]
-  }
 
-};
+module.exports = (env) => {
+  return {
+    mode: env.WEBPACK_BUILD ? "production" : "development",
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "babel-loader"
+            }
+          ]
+        },
+        {
+          test: /\.(jpg|jpeg|png|ico|gif)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                outputPath: "img",
+                name: "[name].[ext]"
+                // name: "[name]-[sha1:hash:7].[ext]"
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(ttf|woff|woff2|eot|otf)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                outputPath: "fonts",
+                name: "[name].[ext]"
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            !env.WEBPACK_BUILD ? "style-loader" : MiniCssExtractPlugin.loader,
+            "css-loader",
+            // "postcss-loader",
+            "sass-loader",
+          ],
+        }
+      ]
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "public/index.html"
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name]-[contenthash:6].css"
+      })
+    ],
+
+    devServer: {
+      open: true
+    }
+
+  };
+}
